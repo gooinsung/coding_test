2e8ba77c266a3df6d45370265e41cf4de2c1d933
gooin <gooinsung96@gmail.com>
2024-02-26 00:09:38 +0900

    20240226

--- src/insung/programers/level_1/no_118666.java
+++ src/insung/programers/level_1/no_118666.java
@@ -1,2 +1,17 @@
-package insung.programers.level_1;public class no_118666 {
+package insung.programers.level_1;
+
+public class no_118666 {
+
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+
+
+  }
+  static class Solution {
+    public String solution(String[] survey, int[] choices) {
+      String answer = "";
+      return answer;
+    }
+  }
+
 }
